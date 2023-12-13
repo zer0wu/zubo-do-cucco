9d295179b1c6b1b0ec873a07c1ed178bceb97fc8
PedroRamprankoliv <153745302+PedroRamprankoliv@users.noreply.github.com>
2023-12-13 20:18:12 -0300

    Add files via upload

---
+++ SerialCommunicator.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "stdint.h"
+#include <windows.h>
+
+class SerialCommunicator {
+public:
+    SerialCommunicator(const char* portName, int baudRate);
+    ~SerialCommunicator();
+
+    bool openPort();
+    void closePort();
+    bool sendData(const uint8_t matrix[8][8][8]);
+    bool receiveData(uint8_t& receivedData);
+    void sendSNC();
+
+private:
+    HANDLE hCom;
+    const char* portName;
+    int baudRate;
+};
