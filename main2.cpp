e0c77cdd4f4e47814f69ba22aad262cf7aec6a66
PedroRamprankoliv <153745302+PedroRamprankoliv@users.noreply.github.com>
2023-12-14 11:53:38 -0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -8,20 +8,35 @@
 using namespace std;
 
 int main() {
-    const char* portName = "COM5";  // Change to your port name
-    int baudRate = 115200;  // Adjust as needed
+    int alternate = 0;
+    const char* portName = "COM5";  // nome da porta
+    int baudRate = 115200;
     Cubo cubo;
     SerialCommunicator communicator(portName, baudRate);
 
     if (communicator.openPort()) {
         cout << " Porta serial aberta ." << std::endl;
-     uint8_t i = 0;
-communicator.sendSNC(i);
-        cout << "SNC enviado." << endl;
-        // Test sending data
 
-        // Test receiving data
- uint8_t receivedData;
+
+while(1){
+        if(alternate == 0)alternate = 1;
+        else alternate = 0;
+switch (alternate){
+
+case 1:
+    cubo.setCubo();
+    break;
+case 0:
+    cubo.resetCubo();
+    break;
+
+}
+
+communicator.sendSNC();
+        cout << "SNC enviado." << endl;
+
+
+uint8_t receivedData;
         if (communicator.receiveData(receivedData)) {
             cout << "Retorno do SNC: 0x" << std::hex << static_cast<int>(receivedData) << endl;
         } else {
@@ -29,7 +44,8 @@ communicator.sendSNC(i);
         }
 
 
-        uint8_t matrixToSend[8][8][8];
+
+  uint8_t matrixToSend[8][8][8];
         for (int x = 0; x < 8; ++x) {
             for (int y = 0; y < 8; ++y) {
                 for (int z = 0; z < 8; ++z) {
@@ -52,16 +68,18 @@ communicator.sendSNC(i);
             cerr << "Falha no envio nos cubos" << endl;
         }
 
+    communicator.ativa();
+    cout << "cubo ativado" << endl;
 
-        communicator.closePort();
+    Sleep(1000);
+
+}
+
+    communicator.closePort();
         cout << "Porta Serial fechada" << endl;
     } else {
         cerr << "Falha em abrir na porta serial " << endl;
     }
-
-
-   // communicator.ativa(i);
-   // cout << "cubo ativado" << endl;
 
 
 
