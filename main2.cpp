f8b47c7b900c33a19b87628d8dd8fe2ac16a66e9
PedroRamprankoliv <153745302+PedroRamprankoliv@users.noreply.github.com>
2023-12-14 00:26:13 -0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,76 +1,45 @@
-#include "cubo.h"
 #include <iostream>
 #include "SerialCommunicator.h"
 #include "windows.h"
-void displayMatrix(const Cubo& cubo) {
-    std::cout << "Current state of the cubo:" << std::endl;
-    for (int x = 0; x < 8; ++x) {
-        for (int y = 0; y < 8; ++y) {
-            for (int z = 0; z < 8; ++z) {
-                std::cout << (cubo.matrix_led[x][y][z] ? "*" : "-") << " ";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-    }
-}
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-    Cubo cubo;
-SerialCommunicator comunica("COM5", 115200);
+    const char* portName = "COM5";  // Change to your port name
+    int baudRate = 115200;  // Adjust as needed
 
-comunica.openPort();
+    SerialCommunicator communicator(portName, baudRate);
 
-    // Test resetCubo
-    std::cout << "Testing resetCubo..." << std::endl;
-    cubo.resetCubo();
-    comunica.sendData(cubo.matrix_led);
-Sleep(1000);
+    if (communicator.openPort()) {
+        cout << "Serial port opened successfully." << std::endl;
 
+        // Test sending data
+        uint8_t testMatrix[8][8][8] = {};  // Replace with your test data
+        if (communicator.sendData(testMatrix)) {
+            cout << "Data sent successfully." << std::endl;
+        } else {
+            cerr << "Failed to send data." << std::endl;
+        }
 
-    // Test setCubo
-    std::cout << "Testing setCubo..." << std::endl;
-    cubo.setCubo();
-    comunica.sendSNC();
-    comunica.sendData(cubo.matrix_led);
-Sleep(1000);
+        // Test receiving data
+        uint8_t receivedData;
+        if (communicator.receiveData(receivedData)) {
+            cout << "Received data: 0x" << std::hex << static_cast<int>(receivedData) << endl;
+        } else {
+            cerr << "Failed to receive data." << endl;
+        }
 
+        // Test sending SNC command
+        uint8_t i = 0;  // Adjust as needed
+        communicator.sendSNC(i);
+        cout << "SNC command sent successfully." << std::endl;
 
-    // Test ledOn and ledOff
-    std::cout << "Testing ledOn and ledOff..." << std::endl;
-    cubo.ledOn(2, 3, 4);
-
-    comunica.sendSNC();
-    comunica.sendData(cubo.matrix_led);
-    Sleep(1000);
-
-
-    cubo.ledOff(2, 3, 4);
-    comunica.sendSNC();
-    comunica.sendData(cubo.matrix_led);
-    Sleep(1000);
-
-
-cubo.resetCubo();
-    // Test criaParticula
-    std::cout << "Testing criaParticula..." << std::endl;
-
-
-    cubo.criaParticula(1, 2);
-    comunica.sendSNC();
-    comunica.sendData(cubo.matrix_led);
-    Sleep(1000);
-
-
-
-    // Test caiParticula
-    std::cout << "Testing caiParticula..." << std::endl;
-    cubo.caiParticula();
-    comunica.sendSNC();
-    comunica.sendData(cubo.matrix_led);
-    Sleep(1000);
-
-
+        communicator.closePort();
+        cout << "Serial port closed." << std::endl;
+    } else {
+        cerr << "Failed to open the serial port." << std::endl;
+    }
 
     return 0;
 }
