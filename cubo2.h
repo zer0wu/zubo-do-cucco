3f17fa7f8a25eb026616e1144466d3ad6f00b177
PedroRamprankoliv <153745302+PedroRamprankoliv@users.noreply.github.com>
2023-12-14 11:01:00 -0300

    Update cubo2.h

--- cubo2.h
+++ cubo2.h
@@ -1,34 +1,69 @@
-#ifndef CUBO_H
-#define CUBO_H
+#include <iostream>
+#include "SerialCommunicator.h"
+#include "windows.h"
+#include "Cubo.h"
+#include <iomanip>
+#include <cstdlib>
 
-#include "stdint.h"
+using namespace std;
 
-class Cubo
-{
-public:
-    Cubo();
-    virtual ~Cubo();
-    uint8_t matrix_led[8][8][8];    //matriz que define o estado de cada LED
-void resetCubo();           //zera o valor de todos os LEDs
+int main() {
+    const char* portName = "COM5";  // Change to your port name
+    int baudRate = 115200;  // Adjust as needed
+    Cubo cubo;
+    SerialCommunicator communicator(portName, baudRate);
 
-    void ledOn(int x, int y, int z);  //liga um led especifico da matriz
+    if (communicator.openPort()) {
+        cout << " Porta serial aberta ." << std::endl;
+     uint8_t i = 0;
+communicator.sendSNC(i);
+        cout << "SNC enviado." << endl;
+        // Test sending data
 
-    void ledOff(int x, int y, int z); //desliga um led especifico da matriz
-
-    void setCubo();
-
-    void criaParticula( int x, int y);
-
-    void caiParticula ();
-    uint8_t Getmatrizdata(int x, int y, int z) const;
-
-
-
-protected:
+        // Test receiving data
+ uint8_t receivedData;
+        if (communicator.receiveData(receivedData)) {
+            cout << "Retorno do SNC: 0x" << std::hex << static_cast<int>(receivedData) << endl;
+        } else {
+            cerr << "Failed to receive data." << endl;
+        }
 
 
+        uint8_t matrixToSend[8][8][8];
+        for (int x = 0; x < 8; ++x) {
+            for (int y = 0; y < 8; ++y) {
+                for (int z = 0; z < 8; ++z) {
+                    matrixToSend[x][y][z] = cubo.Getmatrizdata(x, y, z);
+                }
+            }
+        }
 
-private:
-};
+        if (communicator.sendData(matrixToSend)) {
+            cout << "Dados enviados." << endl;
+        } else {
+            cerr << "Falha no envio de dados" << endl;
+        }
 
-#endif // CUBO_H
+
+ uint8_t receivedData2;
+        if (communicator.receiveData2(receivedData2)) {
+            cout << "Retorno da matriz: 0x" << std::hex << static_cast<int>(receivedData2) << endl;
+        } else {
+            cerr << "Falha no envio nos cubos" << endl;
+        }
+
+
+        communicator.closePort();
+        cout << "Porta Serial fechada" << endl;
+    } else {
+        cerr << "Falha em abrir na porta serial " << endl;
+    }
+
+
+   // communicator.ativa(i);
+   // cout << "cubo ativado" << endl;
+
+
+
+    return 0;
+}
